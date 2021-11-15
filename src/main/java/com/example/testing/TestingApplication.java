2ce0ed2e288c30f203c7d80715f3e94bb138d199
Patrick Lam (ASW - Senior System Analyst <Patrick.Lam@aswatson.com>
2021-11-15 11:03:33 +0800

    first commit

---
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -0,0 +1,14 @@
+package com.example.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		System.out.println("agag");
+		SpringApplication.run(TestingApplication.class, args);
+	}
+
+}
